03b5f517d080127328261242f01495aebff70a0f
Davi <drmedeiros.gok@rd.com.br>
2021-12-13 22:09:48 -0300

    finishes login screen layout

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,14 +1,52 @@
 import React from 'react';
 import logo from '../../assets/images/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-import { Container, Image, Title, Description } from './styles';
+import {
+  Container,
+  Content,
+  Form,
+  Image,
+  Title,
+  Description,
+  Label,
+  Separator,
+} from './styles';
 
 function Login() {
   return (
     <Container>
-      <Image source={logo} />
-      <Title>Buscar usuário</Title>
-      <Description>Crie sua conta através do seu usuário do GitHub</Description>
+      <Content>
+        <Image source={logo} />
+
+        <Form>
+          <Title>Buscar usuário</Title>
+
+          <Separator height={10} />
+
+          <Description>
+            Crie sua conta através do seu usuário do GitHub
+          </Description>
+
+          <Separator height={32} />
+
+          <Input
+            icon="account-circle"
+            onChangeText={() => null}
+            placeholder="@username"
+          />
+
+          <Separator height={24} />
+
+          <Button label="Cadastrar" onPress={() => null} />
+        </Form>
+
+        <Label>
+          Termos de <Label underline>política</Label> e{' '}
+          <Label underline>privacidade</Label>
+        </Label>
+      </Content>
     </Container>
   );
 }
