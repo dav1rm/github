89960646784a8f1151ff739129ca73235f3adbcc
Davi <drmedeiros.gok@rd.com.br>
2021-12-19 00:05:18 -0300

    add padding and height prop to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,12 +3,13 @@ import { Container, TextButton } from './styles';
 
 interface ButtonProps {
   label: string;
+  height?: number;
   onPress: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onPress }) => {
+const Button: React.FC<ButtonProps> = ({ label, height, onPress }) => {
   return (
-    <Container onPress={onPress}>
+    <Container height={height} onPress={onPress}>
       <TextButton>{label}</TextButton>
     </Container>
   );
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,12 +1,18 @@
 import styled from 'styled-components/native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export const Container = styled.TouchableOpacity.attrs({ activeOpacity: 0.7 })`
-  height: 40px;
+interface ContainerProps {
+  height?: number;
+}
+export const Container = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.7,
+})<ContainerProps>`
+  height: ${({ height }) => height ?? 40}px;
   border-radius: 100px;
   background: #000;
   align-items: center;
   justify-content: center;
+  padding: 0 20px;
 `;
 
 export const TextButton = styled.Text`
