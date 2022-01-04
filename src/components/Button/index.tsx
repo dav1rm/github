08bd09b5562eb45933661e4f566ae8715ba40d19
Davi <drmedeiros.gok@rd.com.br>
2022-01-04 15:24:19 -0300

    add light mode to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,13 +4,14 @@ import { Container, TextButton } from './styles';
 interface ButtonProps {
   label: string;
   height?: number;
+  light?: boolean;
   onPress: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, height, onPress }) => {
+const Button: React.FC<ButtonProps> = ({ label, height, light, onPress }) => {
   return (
-    <Container height={height} onPress={onPress}>
-      <TextButton>{label}</TextButton>
+    <Container height={height} light={light} onPress={onPress}>
+      <TextButton light={light}>{label}</TextButton>
     </Container>
   );
 };
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -3,23 +3,28 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 interface ContainerProps {
   height?: number;
+  light?: boolean;
 }
 export const Container = styled.TouchableOpacity.attrs({
   activeOpacity: 0.7,
 })<ContainerProps>`
   height: ${({ height }) => height ?? 40}px;
   border-radius: 100px;
-  background: #000;
+  background: ${({ light }) => (light ? '#fff' : '#000')};
   align-items: center;
   justify-content: center;
   padding: 0 20px;
 `;
 
-export const TextButton = styled.Text`
+interface TextButtonProps {
+  height?: number;
+  light?: boolean;
+}
+export const TextButton = styled.Text<TextButtonProps>`
   font-family: 'Mulish-Regular';
   font-size: 16px;
   line-height: 20px;
-  color: #fff;
+  color: ${({ light }) => (light ? '#000' : '#fff')};
 `;
 
 export const IconInput = styled(Icon)`
