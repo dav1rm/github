e2afed567a91238114c9fac3c130869d4444aff3
Davi <drmedeiros.gok@rd.com.br>
2022-01-03 22:07:29 -0300

    add tag list in search bar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import ActionButton from '../ActionButton';
 import Input from '../Input';
-import { Container, Separator } from './styles';
+import Tag from '../Tag';
+import { BarContainer, Container, Separator, TagsContainer } from './styles';
 
 interface SearchBarProps {}
 
@@ -9,46 +10,53 @@ const SearchBar: React.FC<SearchBarProps> = () => {
   const [type, setType] = useState('search');
   return (
     <Container>
-      {type === 'filter' && (
-        <>
-          <ActionButton
-            onPress={() => setType('search')}
-            iconName="search"
-            height={40}
-            width={48}
-            iconSize={24}
-            iconColor="#7E7E7E"
-            bgColor="#fff"
-            rounded={false}
-            hasShadow
-          />
-          <Separator width={8} />
-        </>
-      )}
-      <Input
-        background="#fff"
-        icon={type === 'search' ? 'search' : 'filter-list'}
-        placeholder={`${
-          type === 'search' ? 'Buscar' : 'Filtrar'
-        } um repositório...`}
-        hasShadow
-      />
-      {type === 'search' && (
-        <>
-          <Separator width={8} />
-          <ActionButton
-            height={40}
-            width={48}
-            iconSize={24}
-            onPress={() => setType('filter')}
-            iconName="filter-list"
-            iconColor="#7E7E7E"
-            bgColor="#fff"
-            rounded={false}
-            hasShadow
-          />
-        </>
-      )}
+      <BarContainer>
+        {type === 'filter' && (
+          <>
+            <ActionButton
+              onPress={() => setType('search')}
+              iconName="search"
+              height={40}
+              width={48}
+              iconSize={24}
+              iconColor="#7E7E7E"
+              bgColor="#fff"
+              rounded={false}
+              hasShadow
+            />
+            <Separator width={8} />
+          </>
+        )}
+        <Input
+          background="#fff"
+          icon={type === 'search' ? 'search' : 'filter-list'}
+          placeholder={`${
+            type === 'search' ? 'Buscar' : 'Filtrar'
+          } um repositório...`}
+          hasShadow
+        />
+        {type === 'search' && (
+          <>
+            <Separator width={8} />
+            <ActionButton
+              height={40}
+              width={48}
+              iconSize={24}
+              onPress={() => setType('filter')}
+              iconName="filter-list"
+              iconColor="#7E7E7E"
+              bgColor="#fff"
+              rounded={false}
+              hasShadow
+            />
+          </>
+        )}
+      </BarContainer>
+      <TagsContainer horizontal>
+        {['Front End', 'Java', 'C#'].map((tag: string) => (
+          <Tag label={tag} key={tag} type="add" onPress={() => null} />
+        ))}
+      </TagsContainer>
     </Container>
   );
 };
--- src/components/SearchBar/styles.ts
+++ src/components/SearchBar/styles.ts
@@ -1,10 +1,18 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.View``;
+
+export const BarContainer = styled.View`
   flex-direction: row;
   padding: 8px 16px;
   padding-bottom: 0;
   align-items: center;
+`;
+
+export const TagsContainer = styled.ScrollView`
+  padding: 8px 16px;
+  padding-bottom: 0;
+  margin: 8px 0;
 `;
 
 interface SeparatorProps {
