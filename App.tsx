c9fa47d951ac3d961ea860511d88959333b62534
Davi <drmedeiros.gok@rd.com.br>
2022-01-20 23:37:53 -0300

    add apollo graphql

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Routes } from './src/routes';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: 'Bearer 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',
+  },
+});
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 };
 
