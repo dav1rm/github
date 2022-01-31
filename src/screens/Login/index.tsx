918ba36835eea3497935fd9240461f56f181843d
Davi <drmedeiros.gok@rd.com.br>
2022-01-30 23:21:34 -0300

    finalizes login screen

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
 import logo from '../../assets/images/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import { useNavigation } from '@react-navigation/native';
+import { useStorage } from '../../hooks/storage';
+import { LoginScreenProp } from '../../routes/types';
+import { GET_USER_INFO } from '../../services/graphql/queries';
 import {
   Container,
   Content,
@@ -15,6 +20,35 @@ import {
 } from './styles';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const { updateUsers, users } = useStorage();
+  const navigation = useNavigation<LoginScreenProp>();
+  const [getUserInfo, { data, loading, error }] = useLazyQuery(GET_USER_INFO);
+
+  const description = navigation.canGoBack()
+    ? 'Adicione seus novos usuários do GitHub'
+    : 'Crie sua conta através do seu usuário do GitHub';
+
+  useEffect(() => {
+    if (!loading && !error && !!data) {
+      if (data?.user) {
+        updateUsers([...users, data?.user]);
+
+        if (navigation.canGoBack()) {
+          navigation.goBack();
+        }
+      }
+    }
+  }, [data, error, loading, navigation, updateUsers, users]);
+
+  async function handleSearchUser() {
+    if (!username) {
+      return;
+    }
+
+    await getUserInfo({ variables: { username } });
+  }
+
   return (
     <Container>
       <Content>
@@ -25,21 +59,25 @@ function Login() {
 
           <Separator height={10} />
 
-          <Description>
-            Crie sua conta através do seu usuário do GitHub
-          </Description>
+          <Description>{description}</Description>
 
           <Separator height={32} />
 
           <Input
             icon="account-circle"
-            onChangeText={() => null}
+            value={username}
+            onChangeText={text => setUsername(text)}
             placeholder="@username"
+            autoCapitalize="none"
+            autoCorrect={false}
           />
 
           <Separator height={24} />
 
-          <Button label="Cadastrar" onPress={() => null} />
+          <Button
+            label={loading ? 'Loading' : 'Cadastrar'}
+            onPress={handleSearchUser}
+          />
         </Form>
 
         <Label>
